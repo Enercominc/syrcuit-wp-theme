8f6acdde2fb73ed81e42a5878bde09b3da1cacde
bradmkjr <brad@ucanbonline.net>
2022-05-09 16:42:13 -0600

    changed header

--- lib/class-theme-layout.php
+++ lib/class-theme-layout.php
@@ -143,7 +143,7 @@ class SyrcuitThemeLayout extends Method_Layout {
 														
 										</div>
 										<div class="col-12 p-4">
-											' . $this->get_headline( '_syrcuit_' . $prefix . '_headline', '<h1 class="syrcuit-h1 py-5">', '</h1>' ) . '
+											' . $this->get_headline( '_syrcuit_' . $prefix . '_headline', '<h1 class="syrcuit-h1 pt-5 pb-4">', '</h1>' ) . '
 											' . $this->get_headline( '_syrcuit_' . $prefix . '_sub_headline', '<h2 class="syrcuit-h1 py-5">', '</h2>' ) . '
 											' . $this->get_content( '_syrcuit_' . $prefix . '_content', '<div class="copy py-4">', '</div>' ) . '
 										</div>
